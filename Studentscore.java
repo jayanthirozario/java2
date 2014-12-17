f256a9c6664253639c4515ea9fe53841e2ff16ca
jayanthirozario <jayanthirozario@yahoo.com>
2014-12-17 10:24:18 -0800

    studentscore

---
+++ Studentscore.java
@@ -0,0 +1,18 @@
+
+public class Studentscore
+{
+    // instance variables - replace the example below with your own
+    public static void main (String args[]){ 
+        
+        Studentclass grade1=new Studentclass();
+        Studentclass grade2= new Studentclass();
+        grade1.setinfo("Arul",32,80);
+        grade2.setinfo("Vimal",31,90);
+        
+        grade1.getgrades();
+        grade2.getgrades();
+
+    
+        
+    }
+}
